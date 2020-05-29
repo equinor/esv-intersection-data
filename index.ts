ee3edfe9c44806c42b0e68761db09eaf57bc6106
andreabrambilla <abramb@equinor.com>
2020-05-29 08:12:38 +0200

    Make it possible to import this folder directly

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * as holeSizeData from './holesize.json';
+export * as casingData from './casings.json';
+export * as cementData from './cement.json';
+export * as seismicColorMap from './seismic-colormap.json';
+export * as poslog from './poslog.json';
